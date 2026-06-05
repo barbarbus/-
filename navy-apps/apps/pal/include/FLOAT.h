001947d95d5efe9347299efeb80a75e27783fef0
tracer-ics2017 <tracer@njuics.org>
2026-06-05 02:54:42 -0700

    >  run 2312390 czc Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:54:42 up 1 day, 22:11,  1 user,  load average: 0.21, 0.19, 0.12 ac82af17343b889fc6a7de373d67c238e6495d05

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -1,28 +1,24 @@
 #ifndef __FLOAT_H__
 #define __FLOAT_H__
 
-#include "assert.h"
+#include <stdint.h>
 
-typedef int FLOAT;
+typedef int32_t FLOAT;
 
 static inline int F2int(FLOAT a) {
-  assert(0);
-  return 0;
+  return a >> 16;
 }
 
 static inline FLOAT int2F(int a) {
-  assert(0);
-  return 0;
+  return (FLOAT)a << 16;
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+  return a * b;
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+  return a / b;
 }
 
 FLOAT f2F(float);
--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -1,35 +1,55 @@
 #include "FLOAT.h"
 #include <stdint.h>
-#include <assert.h>
+#include <string.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  return (FLOAT)(((int64_t)a * b) >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  return (FLOAT)(((int64_t)a << 16) / b);
 }
 
 FLOAT f2F(float a) {
-  /* You should figure out how to convert `a' into FLOAT without
-   * introducing x87 floating point instructions. Else you can
-   * not run this code in NEMU before implementing x87 floating
-   * point instructions, which is contrary to our expectation.
-   *
-   * Hint: The bit representation of `a' is already on the
-   * stack. How do you retrieve it to another variable without
-   * performing arithmetic operations on it directly?
-   */
+  union {
+    float f;
+    uint32_t u;
+  } uf;
+  uf.f = a;
 
-  assert(0);
-  return 0;
+  uint32_t bits = uf.u;
+  uint32_t sign = bits >> 31;
+  int exp = (int)((bits >> 23) & 0xff) - 127;
+  uint32_t frac = bits & 0x7fffffu;
+
+  if (exp == 128) {
+    return 0;
+  }
+
+  int64_t val;
+  if (exp < 0) {
+    if (exp < -16) {
+      return 0;
+    }
+    val = (int64_t)frac >> (23 - exp - 16);
+  } else {
+    frac |= 0x800000u;
+    int sh = exp + 16 - 23;
+    if (sh >= 0) {
+      val = (int64_t)frac << sh;
+    } else {
+      val = (int64_t)frac >> (-sh);
+    }
+  }
+
+  if (sign) {
+    val = -val;
+  }
+  return (FLOAT)val;
 }
 
 FLOAT Fabs(FLOAT a) {
-  assert(0);
-  return 0;
+  return a < 0 ? -a : a;
 }
 
 /* Functions below are already implemented */
@@ -40,7 +60,7 @@ FLOAT Fsqrt(FLOAT x) {
   do {
     dt = F_div_int((F_div_F(x, t) - t), 2);
     t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+  } while (Fabs(dt) > f2F(1e-4f));
 
   return t;
 }
@@ -51,9 +71,9 @@ FLOAT Fpow(FLOAT x, FLOAT y) {
 
   do {
     t2 = F_mul_F(t, t);
-    dt = (F_div_F(x, t2) - t) / 3;
+    dt = F_div_int((F_div_F(x, t2) - t), 3);
     t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+  } while (Fabs(dt) > f2F(1e-4f));
 
   return t;
 }
